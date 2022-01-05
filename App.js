8f5c81d55d3462737c787a3c6f5265bf29b66108
Isaiah Jenkins <icjenkins@dons.usfca.edu>
2022-01-05 15:28:21 -0800

    Setting up RestaurantsContext

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import {theme} from './src/infrastructure/theme';
 import {RestaurantsScreen} from './src/features/restaurants/screens/restaurants.screen';
 import {SafeArea} from './src/components/utility/safe-area.component';
+import {RestaurantsContextProvider} from './src/services/restaurants/restaurants.context';
+
 const Tab = createBottomTabNavigator();
 
 const TAB_ICON = {
@@ -43,13 +45,15 @@ const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <Tab.Navigator screenOptions={createScreenOptions}>
-            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-            <Tab.Screen name="Map" component={Map} />
-            <Tab.Screen name="Settings" component={Settings} />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <RestaurantsContextProvider>
+          <NavigationContainer>
+            <Tab.Navigator screenOptions={createScreenOptions}>
+              <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+              <Tab.Screen name="Map" component={Map} />
+              <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </RestaurantsContextProvider>
       </ThemeProvider>
       <StatusBar style="auto" />
     </>
