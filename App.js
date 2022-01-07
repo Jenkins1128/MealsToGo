80c624b67868ece65dcb77bda076ce5ddb195edc
Isaiah Jenkins <icjenkins@dons.usfca.edu>
2022-01-07 12:58:57 -0800

    Restructed navigation & added modal view for detail info card

--- App.js
+++ App.js
@@ -1,46 +1,11 @@
 import React from 'react';
 import {ThemeProvider} from 'styled-components/native';
-import {StatusBar, Text} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import {StatusBar} from 'react-native';
 
 import {theme} from './src/infrastructure/theme';
-import {RestaurantsScreen} from './src/features/restaurants/screens/restaurants.screen';
-import {SafeArea} from './src/components/utility/safe-area.component';
 import {RestaurantsContextProvider} from './src/services/restaurants/restaurants.context';
 import {LocationContextProvider} from './src/services/location/location.context';
-
-const Tab = createBottomTabNavigator();
-
-const TAB_ICON = {
-  Restaurants: 'md-restaurant',
-  Map: 'md-map',
-  Settings: 'md-settings',
-};
-
-const Settings = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
-
-const Map = () => (
-  <SafeArea>
-    <Text>Map</Text>
-  </SafeArea>
-);
-
-const createScreenOptions = ({route}) => {
-  const iconName = TAB_ICON[route.name];
-  return {
-    tabBarIcon: ({size, color}) => (
-      <Ionicons name={iconName} size={size} color={color} />
-    ),
-    tabBarActiveTintColor: 'tomato',
-    tabBarInactiveTintColor: 'gray',
-  };
-};
+import {Navigation} from './src/infrastructure/navigation';
 
 const App = () => {
   return (
@@ -48,13 +13,7 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <LocationContextProvider>
           <RestaurantsContextProvider>
-            <NavigationContainer>
-              <Tab.Navigator screenOptions={createScreenOptions}>
-                <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-                <Tab.Screen name="Map" component={Map} />
-                <Tab.Screen name="Settings" component={Settings} />
-              </Tab.Navigator>
-            </NavigationContainer>
+            <Navigation />
           </RestaurantsContextProvider>
         </LocationContextProvider>
       </ThemeProvider>
