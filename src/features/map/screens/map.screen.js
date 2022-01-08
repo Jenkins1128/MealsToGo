c0d103140f5f053a2eb71f2c6b4912803c434841
Isaiah Jenkins <icjenkins@dons.usfca.edu>
2022-01-07 23:58:51 -0800

    Updated map to set region for city

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,4 +1,45 @@
 import React, {useContext, useState, useEffect} from 'react';
 import MapView from 'react-native-maps';
+import styled from 'styled-components/native';
+import {LocationContext} from '../../../services/location/location.context';
+import {RestaurantsContext} from '../../../services/restaurants/restaurants.context';
+import {Search} from '../components/search.component';
 
-export const MapScreen = () => <MapView style={{height: '100%'}} />;
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
+
+export const MapScreen = () => {
+  const {location} = useContext(LocationContext);
+  const {restaurants = []} = useContext(RestaurantsContext);
+
+  //for zooming on the map
+  const [latDelta, setLatDelta] = useState(0);
+
+  const {viewport, lat, lng} = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
+  return (
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}>
+        {restaurants.map(restaurant => {
+          return null;
+        })}
+      </Map>
+    </>
+  );
+};
