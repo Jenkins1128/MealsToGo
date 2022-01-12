832c592393b5bde093265898632aa5deeeeda060
Isaiah Jenkins <icjenkins@dons.usfca.edu>
2022-01-11 22:34:08 -0800

    Firebase and Auth added!

--- App.js
+++ App.js
@@ -3,22 +3,26 @@ import {ThemeProvider} from 'styled-components/native';
 import {StatusBar} from 'react-native';
 
 import {theme} from './src/infrastructure/theme';
+import {Navigation} from './src/infrastructure/navigation';
+
 import {RestaurantsContextProvider} from './src/services/restaurants/restaurants.context';
 import {LocationContextProvider} from './src/services/location/location.context';
 import {FavoritesContextProvider} from './src/services/favorites/favorites.context';
-import {Navigation} from './src/infrastructure/navigation';
+import {AuthenticationContextProvider} from './src/services/authentication/authentication.context';
 
 const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavoritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavoritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavoritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavoritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <StatusBar style="auto" />
     </>
