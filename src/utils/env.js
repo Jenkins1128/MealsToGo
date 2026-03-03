e557e8e665f129458e91fe383df2ca4816c8110f
Isaiah Jenkins <35716724+Jenkins1128@users.noreply.github.com>
2026-03-03 11:08:33 -0800

    Added patch-package, postinstall-postinall,fFixed Podfile issues and nodemodule archive boost issue

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,6 +2,6 @@ const localHost =
   'https://76a6-99-13-224-111.ngrok.io/mealstogo-52272/us-central1';
 const liveHost = 'https://us-central1-mealstogo-52272.cloudfunctions.net';
 
-export const isMock = true;
+export const isMock = false;
 export const isDevelopment = process.env.NODE_ENV === 'development';
-export const host = isDevelopment ? localHost : liveHost;
+export const host = liveHost;
